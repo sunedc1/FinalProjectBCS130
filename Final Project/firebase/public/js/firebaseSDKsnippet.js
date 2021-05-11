866f3c010fd5dd6168cbc0bf309514057973f4e8
sunedc1 <84050232+sunedc1@users.noreply.github.com>
2021-05-11 19:46:05 -0400

    Add files via upload

---
+++ Final Project/firebase/public/js/firebaseSDKsnippet.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyByO6jGRndpPtvqMSfUi-UsM5RSm3vzuEM",
+    authDomain: "bcs130-final-project-63407.firebaseapp.com",
+    projectId: "bcs130-final-project-63407",
+    storageBucket: "bcs130-final-project-63407.appspot.com",
+    messagingSenderId: "133293894024",
+    appId: "1:133293894024:web:ea97ecd91a1492eb1b323d",
+    measurementId: "G-J4MCKL5P5F"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
